b406edf092007aede549c144af113d4d5485c389
kirsley <kirsley@gmail.com>
2017-03-27 14:59:25 +0200

    Add new Project

---
+++ GatRenat/GatRenat.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author kirsley
+ *
+ */
+public class GatRenat {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
