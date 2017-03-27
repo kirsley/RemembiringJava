916ad05c19db89511b8aaa99a9d449327c4f4f8d
kirsley <kirsley@gmail.com>
2017-03-27 16:05:18 +0200

    Exer 1.1

--- GatRenat/GatRenat.java
+++ GatRenat/GatRenat.java
@@ -8,7 +8,10 @@
  */
 public class GatRenat {
 	private int vides = 1;
-	//vides disponibles del gat Renat
+	private enum Estat {
+		dret,assegut,estirat
+	}
+	Estat estat = Estat.dret;
 	public int getVides(){
 		return this.vides;
 	}
@@ -19,6 +22,51 @@ public class GatRenat {
 			System.out.println("Num de vides erroni");
 		}
 	}
-	public static void main(String[] args) {
+
+	public String getEstat() {
+		return this.estat.toString();
+	}
+	public void setEstat(String estat) {
+		this.estat = Estat.valueOf(estat);
+	}
+	public boolean estaViu(){
+		return this.vides > 0;
+	}
+	
+	public boolean estaDret(){
+		return this.getEstat().toString() == "dret";
+	}
+	boolean estaAssegut(){
+		return this.getEstat().toString() == "assegut";
+	}
+	boolean estaEstirat(){
+		return this.getEstat().toString() == "estirat";
+	}
+	
+	public void aixecat(){
+		if ( ! this.estaDret()){
+			System.out.println("M'aixeco");
+			this.setEstat("dret");
+		} else {
+			System.out.println("Ja ho estava!");
+		}
+	}
+	
+	public void seu(){
+		if ( !this.estaAssegut()){
+			System.out.println("M'assec");
+			this.setEstat("assegut");
+		} else {
+			System.out.println("Ja ho estava!");
+		}
+	}
+	
+	public void estirat(){
+		if(!this.estaEstirat()){
+			System.out.println("M'estiro");
+			this.setEstat("estirat");
+		} else {
+			System.out.println("Ja ho estava!");
+		}
 	}
 }
