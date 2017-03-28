2a24cfcf52db72a0af9600a48d0a18e4b815ed41
kirsley <kirsley@gmail.com>
2017-03-28 17:24:11 +0200

    Added Todo task

--- PedaleraApi/src/Sesion.java
+++ PedaleraApi/src/Sesion.java
@@ -3,6 +3,7 @@ public class Sesion {
 	private int segundos;
 	private int count;
 	private double calorias;
+	//TODO Add fecha as a Date Type
 	
 	public Sesion(int min,int sec,int count,double calorias){
 		this.segundos = (min * 60) + sec;
@@ -13,25 +14,7 @@ public class Sesion {
 	public int getSegundos() {
 		return segundos;
 	}
-	public String toTime(){
-		int mins = this.getSegundos() / 60;
-		int secs = this.getSegundos() % 60;
-		String mins1;
-		String secs1;
-		if (mins <10){
-			mins1="0" + String.valueOf(mins);
-		} else {
-			mins1=String.valueOf(mins);
-		}
-		
-		if (secs <10){
-            secs1="0" + String.valueOf(secs);
-		} else {
-			secs1=String.valueOf(secs);
-		}
-		
-		return mins1 + ":" + secs1;
-	}
+
 	public void setSegundos(int segundos) {
 		this.segundos = segundos;
 	}
@@ -51,4 +34,31 @@ public class Sesion {
 	public void setCalorias(double calorias) {
 		this.calorias = calorias;
 	}
+
+	/*
+	 * Returns a String with the time formatted mm:ss
+	 */
+	public String toTime(){
+		int mins = this.getSegundos() / 60;
+		int secs = this.getSegundos() % 60;
+		String mins1;
+		String secs1;
+		if (mins <10){
+			mins1="0" + String.valueOf(mins);
+		} else {
+			mins1=String.valueOf(mins);
+		}
+		
+		if (secs <10){
+            secs1="0" + String.valueOf(secs);
+		} else {
+			secs1=String.valueOf(secs);
+		}
+		
+		return mins1 + ":" + secs1;
+	}
 }
+
+
+
+
