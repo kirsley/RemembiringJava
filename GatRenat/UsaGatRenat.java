40286bd437a377e44314e07213cb5985928ef99a
kirsley <kirsley@gmail.com>
2017-12-11 22:06:45 +0100

    Add Changes

--- GatRenat/UsaGatRenat.java
+++ GatRenat/UsaGatRenat.java
@@ -12,7 +12,6 @@ public class UsaGatRenat {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		GatRenat renat = new GatRenat();
 		System.out.println("El Gat Renat esta " + renat.getEstat());
