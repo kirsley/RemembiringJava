92e3c57f3527fe42908875a0ee08d55f0f149f79
kirsley <kirsley@gmail.com>
2018-02-04 22:38:57 +0100

    Change to test Netbeans

--- RememberingJava/src/Remember.java
+++ RememberingJava/src/Remember.java
@@ -3,7 +3,7 @@ public class Remember {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Buenas");
+		System.out.println("Buenas, que tal?");
 	}
 
 }
